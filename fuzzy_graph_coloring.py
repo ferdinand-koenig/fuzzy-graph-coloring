225f81d03f41b887aea27082c0ce1bda79baaa56
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2022-01-29 11:44:34 +0100

    Add test graph 1

--- fuzzy_graph_coloring.py
+++ fuzzy_graph_coloring.py
@@ -1,8 +1,18 @@
 __version__ = "0.1.0"
 
+import matplotlib.pyplot as plt
+import networkx as nx
+
 
 def main():
     print("Hello World")
+
+
+def draw_weighted_graph(graph: nx.Graph):
+    pos = nx.spring_layout(graph)
+    nx.draw(graph, pos, labels={node: node for node in graph.nodes()})
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, "weight"))
+    plt.show()
 
 
 if __name__ == '__main__':
--- tests/test_fuzzy_graph_coloring.py
+++ tests/test_fuzzy_graph_coloring.py
@@ -1,5 +1,18 @@
+import networkx as nx
+
 from fuzzy_graph_coloring import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def build_test_graph_1():
+    TG1 = nx.Graph()
+    TG1.add_edge(1, 2, weight=0.7)
+    TG1.add_edge(1, 3, weight=0.8)
+    TG1.add_edge(1, 4, weight=0.5)
+    TG1.add_edge(2, 3, weight=0.3)
+    TG1.add_edge(2, 4, weight=0.4)
+    TG1.add_edge(3, 4, weight=1.0)
+    return TG1
