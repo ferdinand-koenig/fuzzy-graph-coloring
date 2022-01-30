b579954aa3b209abf00e523b2f85798944d436b8
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2022-01-30 10:45:09 +0100

    Add bruteforce_fuzzy_color function

--- fuzzy_graph_coloring.py
+++ fuzzy_graph_coloring.py
@@ -1,5 +1,7 @@
 __version__ = "0.1.0"
 
+import itertools
+
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -12,7 +14,46 @@ def fuzzy_color(graph: nx.Graph):
     return {}
 
 
+def bruteforce_fuzzy_color(graph: nx.Graph):
+    """
+    Finds the minimal k-coloring for a fuzzy graph by bruteforce all possible color assignments.
+    Excludes the cases k=1 and k=number of nodes since the degree of total incompatibility is 1 and 0.
+
+    :param graph: Fuzzy NetworkX graph
+    :return: dict containing minimal k-colorings (excluding k=1 and k=n)
+    """
+    colors = range(1, graph.number_of_nodes() + 1)
+    color_assignments = itertools.product(*[colors] * graph.number_of_nodes())
+    weight_sum = graph.size(weight="weight")
+    colorings = {}
+    for color_assignment in color_assignments:
+        if len(set(color_assignment)) in [1, graph.number_of_nodes()]:
+            continue
+        total_incompatibility = 0
+        for color in colors:
+            nodes_with_color = [index + 1 for index, element in enumerate(color_assignment) if element == color]
+            incompatible_edges = [(u, v) for (u, v) in graph.edges() if u in nodes_with_color and v in nodes_with_color]
+            for ie in incompatible_edges:
+                total_incompatibility += graph[ie[0]][ie[1]]["weight"]
+        k = len(set(color_assignment))
+        score = 1 - (total_incompatibility / weight_sum)
+        if k not in colorings.keys():
+            colorings[k] = {}
+            colorings[k]["coloring"] = {c: color_assignment[c - 1] for c in colors}
+            colorings[k]["score"] = score
+        elif score > colorings[k]["score"]:
+            colorings[k]["coloring"] = {c: color_assignment[c - 1] for c in colors}
+            colorings[k]["score"] = score
+    return colorings
+
+
 def draw_weighted_graph(graph: nx.Graph):
+    """
+    Plots a given NetworkX graph and labels edges according to their assigned weight.
+
+    :param graph: NetworkX graph
+    :return: None
+    """
     pos = nx.circular_layout(graph)
     nx.draw(graph, pos, labels={node: node for node in graph.nodes()})
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, "weight"))
